070ecba9a6d69d70b250f19c1a90aa275f131618
bipinjlimbu <bipinlimbu712@gmail.com>
2025-09-29 20:28:44 +0545

    Routes File Created

---
+++ src/routes.jsx
@@ -0,0 +1,31 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "./Layout/mainLayout";
+import ListUser from "./components/ListUser";
+import CreateUser from "./components/CreateUser";
+import EditUser from "./components/EditUser";
+
+const routes = createBrowserRouter([
+  {
+    element: <MainLayout />, // layout wrapper
+    children: [
+      {
+        path: '/',
+        element: <ListUser />,
+      },
+      {
+        path: '/create',
+        element: <CreateUser />,
+      },
+      {
+        path: '/edit/:id',
+        element: <EditUser />,
+      },
+      {
+        path: '*',
+        element: <div>404 Not Found</div>,
+      },
+    ],
+  },
+]);
+
+export default routes;

c4ab289c99d95a9c3bdf95f3955bb38e8de16f59
bipinjlimbu <bipinlimbu712@gmail.com>
2025-09-30 08:26:23 +0545

    handleChange with UseState Done

--- src/components/CreateUser.jsx
+++ src/components/CreateUser.jsx
@@ -4,21 +4,34 @@ export default function CreateUser() {
   const handleSubmit = (e) => {
     e.preventDefault();
   }
+
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    number: "",
+    address: ""
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUser({ ...user, [name]: value });
+  }
+
   return (
     <div>
       <h2 className="componentHead">Create User</h2>
       <form onSubmit={handleSubmit}>
         <label htmlFor="name"> Name: </label>
-        <input type="text" id="name" name="name" required />
+        <input type="text" id="name" name="name" onChange={handleChange}required />
         <br />
         <label htmlFor="email"> Email: </label>
-        <input type="text" id="email" name="email" required />
+        <input type="text" id="email" name="email" onChange={handleChange} required />
         <br />
         <label htmlFor="number"> Number: </label>
-        <input type="text" id="number" name="number" required />
+        <input type="text" id="number" name="number" onChange={handleChange} required />
         <br />
         <label htmlFor="address"> Address: </label>
-        <input type="text" id="address" name="address" required />
+        <input type="text" id="address" name="address" onChange={handleChange} required />
         <br />
         <button type="submit">Create User</button>
       </form>

0a1e0d3a06f3b420e58c595975352138e08a15b1
bipinjlimbu <bipinlimbu712@gmail.com>
2025-10-07 21:47:27 +0545

    DB Connected using PDO and OOPs

--- backend/connectDB.php
+++ backend/connectDB.php
@@ -1,9 +1,19 @@
 <?php
-    $conn = mysqli_connect('localhost', 'root', '', 'react-crud');
-    if(!$conn){
-        die("Connection failed: " . mysqli_connect_error());
-    }
-    else{
-        echo "Connected successfully";
-    }
+	class connectDB {
+		private $server = 'localhost';
+		private $dbname = 'react-crud';
+		private $user = 'root';
+		private $pass = '';
+
+		public function connect() {
+			try {
+				$conn = new PDO('mysql:host=' .$this->server .';dbname=' . $this->dbname, $this->user, $this->pass);
+				$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+				return $conn;
+			} catch (\Exception $e) {
+				echo "Database Error: " . $e->getMessage();
+			}
+		}
+        
+	}
 ?>

c888ab22dd667683da64337aac835e1c762606b8
bipinjlimbu <bipinlimbu712@gmail.com>
2025-10-08 16:13:07 +0545

    Api connection to display users list

--- src/components/ListUser.jsx
+++ src/components/ListUser.jsx
@@ -1,7 +1,50 @@
+import axios from "axios"
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
 export default function ListUser() {
-  return (
-    <div>
-      <h2 className="componentHead">User List</h2>
-    </div>
-  );
+
+    const [users, setUsers] = useState([]);
+    useEffect(() => {
+        getUsers();
+    }, []);
+
+    function getUsers() {
+        axios.get('http://localhost/api/user/').then(function(response) {
+            console.log(response.data);
+            setUsers(response.data);
+        });
+    }
+
+    return (
+        <div className="listUser">
+            <h1>List Users</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Number</th>
+                        <th>Address</th>
+                        <th colSpan={2}>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {users.map((user, key) =>
+                        <tr key={key}>
+                            <td>{user.id}</td>
+                            <td>{user.name}</td>
+                            <td>{user.email}</td>
+                            <td>{user.number}</td>
+                            <td>{user.address}</td>
+                            <td><Link>Edit</Link></td>
+                            <td><button>Delete</button></td>
+                        </tr>
+                    )}
+                    
+                </tbody>
+            </table>
+        </div>
+    )
 }

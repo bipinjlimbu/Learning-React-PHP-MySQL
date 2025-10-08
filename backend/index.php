cec5c14a692f4df40ce95cc9b18d1a673f4a0f6e
bipinjlimbu <bipinlimbu712@gmail.com>
2025-10-08 16:16:08 +0545

    Get method done for selecting data from database

--- backend/index.php
+++ backend/index.php
@@ -9,6 +9,24 @@
 
     $method = $_SERVER['REQUEST_METHOD'];
     switch($method) {
+        case "GET":
+        $sql = "SELECT * FROM user";
+        $path = explode('/', $_SERVER['REQUEST_URI']);
+        if(isset($path[3]) && is_numeric($path[3])) {
+            $sql .= " WHERE id = :id";
+            $stmt = $conn->prepare($sql);
+            $stmt->bindParam(':id', $path[3]);
+            $stmt->execute();
+            $users = $stmt->fetch(PDO::FETCH_ASSOC);
+        } else {
+            $stmt = $conn->prepare($sql);
+            $stmt->execute();
+            $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }
+
+        echo json_encode($users);
+        break;
+
         case "POST":
             $user = json_decode( file_get_contents('php://input') );
             $sql = "INSERT INTO user(id, name, email, number, address, created_at) VALUES(null, :name, :email, :number, :address, :created_at)";
